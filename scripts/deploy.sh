b523f234890cea9c67058ff76c01b70876df3c9a
Will Thomson <will.thomson@gmail.com>
2018-06-06 15:19:07 +0100

    Made the app deployable to GitHub Pages

---
+++ scripts/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ng build --prod --base-href "https://willthomson.github.io/mortgage-calculator/"
+ngh
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  private buyerStatus: string = 'first';
+  public buyerStatus: string = 'first';
   public housePrice: number = 500000;
 
   public salary: number = 60000;
